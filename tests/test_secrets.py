5f69103c392846785d3f37323c7a6bfcec03b443
Sam Washko <sam@protectai.com>
2023-09-26 16:19:08 -0700

    Add OP

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -1,4 +1,7 @@
 import os
 
-def test_scan_pickle_bytes() -> None:
+def test_bw_secret() -> None:
     assert os.environ.get("BW_SECRET_1") == "Secret-Value-123"
+
+def test_op_secret() -> None:
+    assert os.environ.get("OP_SECRET_1") == "IQXya3HSTY9Suwtsp18p"
