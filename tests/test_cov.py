189c50253925337279baa1a332d1517b39d74ff3
Sam Washko <sam@protectai.com>
2023-09-29 14:33:05 -0700

    empty test

--- tests/test_cov.py
+++ tests/test_cov.py
@@ -1,11 +1,12 @@
-import os
-from pathlib import Path
-from modelscan.modelscan import Modelscan
+# import os
+# from pathlib import Path
+# from modelscan.modelscan import Modelscan
 
 
 def test_coverage() -> None:
-    try:
-        ms = Modelscan()
-        ms.scan_path(Path(""))
-    except Exception:
-        pass
+    # try:
+    #     ms = Modelscan()
+    #     ms.scan_path(Path(""))
+    # except Exception:
+    #     pass
+    pass
